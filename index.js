fec8e51494386d20b63bb481585ff6d0c7a14eef
Marcos Martinez <mmartinez@kapital.com>
2026-04-15 12:36:26 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({ status: 'ok' })
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`)
+})
