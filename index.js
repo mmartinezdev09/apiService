e3d7f7e5660880f518dd03d7d26b4ff538e2b890
Marcos Martinez <mmartinez@kapital.com>
2026-04-15 17:01:02 -0600

    [FEATURE] add elements from products

--- index.js
+++ index.js
@@ -13,23 +13,51 @@ app.get('/getProducts', (req, res) => {
       products: 
       [
         {
+          id: 1,
           name: 'Ft-150',
-          prize: 200,
           urlImage: 'https://raw.githubusercontent.com/mmartinezdev09/apiService/main/images/ft_150.jpeg'
         },
         {
+          id: 2,
           name: 'Ft-180',
-          prize: 100,
           urlImage: 'https://raw.githubusercontent.com/mmartinezdev09/apiService/main/images/italika_180.jpeg'
         },
         {
-          name: 'At-110',
-          prize: 80,
+          id: 3,
+          name: 'At-110',        
           urlImage: 'https://raw.githubusercontent.com/mmartinezdev09/apiService/main/images/italika_at_110.jpg'
         }
       ]
     }
   )
+})
+
+//post from products by id
+
+app.post('/getProductById', (req, res) => {
+  const { id } = req.body
+
+  let response = null
+
+  if (id === 1) {
+    response = {
+      name: "Balatas traseras Ft-150",
+      prize: 135.00,
+      imageUrl: ""
+    }
+  } else if (id === 2) {
+    response = {
+      name: "Llantas",
+      prize: 100.0,
+      imageUrl: ""
+    }
+  } else {
+    return res.status(404).json({
+      error: "Producto no encontrado"
+    })
+  }
+
+  res.json(response)
 })
 
 const PORT = process.env.PORT || 3000
