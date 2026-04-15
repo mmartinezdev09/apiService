17e54420e00dc0b36a3a54e0e787f4a36d4901b2
Marcos Martinez <mmartinez@kapital.com>
2026-04-15 12:51:06 -0600

    [FEATURE] add initial message from index js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.json({ status: 'ok' })
+  res.json({ status: 'Bienvenido Rot Crash' })
 })
 
 const PORT = process.env.PORT || 3000
