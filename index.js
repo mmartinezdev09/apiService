ca154abeff121cc887629e81cd40cceb692d45b0
Marcos Martinez <mmartinez@kapital.com>
2026-04-15 13:51:14 -0600

    [FEATURE] add products body into response from path products

--- index.js
+++ index.js
@@ -7,6 +7,28 @@ app.get('/welcome', (req, res) => {
   res.json({ status: 'Bienvenido Rot Crash' })
 })
 
+app.get('/getProducts', (req, res) => {
+  res.json(
+    {
+      products: 
+      [
+        {
+          name: 'Ft-150',
+          prize: 200
+        },
+        {
+          name: 'Ft-180',
+          prize: 100
+        },
+        {
+          name: 'Ft-110',
+          prize: 80
+        }
+      ]
+    }
+  )
+})
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
