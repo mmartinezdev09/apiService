8979ff5598aa53442cd3176edadddff81622c50f
Marcos Martinez <mmartinez@kapital.com>
2026-04-17 10:21:31 -0600

    [FEATURE] add async logs from two seconds

--- index.js
+++ index.js
@@ -14,4 +14,11 @@ const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`)
+ (async ( ) => {
+  setTimeout(() => {
+    // Code to execute after 2 seconds
+    console.log('😍')
+}, 2000);
+
+}) ( )
 })
