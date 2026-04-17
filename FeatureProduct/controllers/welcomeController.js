b2232031a3a76e8854f8ff2698636bdce96ed46f
Marcos Martinez <mmartinez@kapital.com>
2026-04-17 12:31:30 -0600

    [FEATURE] add mongo db into welcome message

--- FeatureProduct/controllers/welcomeController.js
+++ FeatureProduct/controllers/welcomeController.js
@@ -1,11 +1,48 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mmartinezdev09_db_user:t0znJyvgZOhy4xSl@cluster0.o5pdjxo.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri,
+    {
+        serverApi:
+        {
+            version: ServerApiVersion.v1, strict: true, deprecationErrors: true,
+        }
+    }
+);
+
+let db; //connect data base 
+
+async function connectDB() {
+    if (!db) {
+        await client.connect();
+        db = client.db("users");
+        console.log("Mongo conectado");
+    }
+}
 const welcomeResponse = {
     message: 'Bienvenido',
     iconUrl: 'https://raw.githubusercontent.com/mmartinezdev09/apiService/main/images/main/icn_main.jpg'
 }
 
 const WelcomeController = {
-    getWelcomeMessage: (req, res) => {
-        res.json(welcomeResponse)
+    getWelcomeMessage: async (req, res) => {
+        try {
+            await connectDB();
+            const result = await db.collection("users").findOne();
+            res.json(
+                {
+                    message: 'Bienvenido',
+                    iconUrl: 'https://raw.githubusercontent.com/mmartinezdev09/apiService/main/images/main/icn_main.jpg',
+                    data: result.users
+                }
+            );
+        } catch (error) {
+            res.status(500).json(
+                {
+                    error: error.message
+                }
+            );
+        }
     }
 }
 
