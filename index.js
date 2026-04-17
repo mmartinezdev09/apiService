0a9581c4176065777a76825a3f8838fff33cf4ee
Marcos Martinez <mmartinez@kapital.com>
2026-04-17 10:34:27 -0600

    [DEMO] job service

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const axios = require('axios');
 const {ProductController} = require("./FeatureProduct/controllers/productController")
 const {ModelController} = require("./FeatureProduct/controllers/modelController")
 const {WelcomeController} = require("./FeatureProduct/controllers/welcomeController")
@@ -12,16 +13,23 @@ app.post('/getProductById', ProductController.getProductById)
 app.get('/getAllProduct', ProductController.getProducts)
 const PORT = process.env.PORT || 3000
 
+function startJob() {
+    setTimeout(async () => {
+        try {
+            const response = await axios.get('https://apiservice-qwgz.onrender.com/welcome');
+            console.log("Respuesta recibida:", response.data);
+        } catch (error) {
+            console.error("Error al llamar al endpoint:", error.message);
+        } finally {
+            startJob();
+        }
+    }, 5000);
+}
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`)
-})
+  startJob()
+  /*setInterval(()=>{
 
- setTimeout(async () => {
-        console.log("Job started...");
-        
-        // Await your logic (e.g., API call)
-        await doWork(); 
-        
-        console.log("Job finished, scheduling next run.");
-        startJob(); // Schedule the next execution
-    }, 5000);
+  })*/
+})
