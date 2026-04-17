7a315f3e4c21b313b23c5307601ef01e3f0f69c6
Marcos Martinez <mmartinez@kapital.com>
2026-04-17 12:44:15 -0600

    [FEATURE] add models by data base from data

--- FeatureProduct/controllers/modelController.js
+++ FeatureProduct/controllers/modelController.js
@@ -1,29 +1,42 @@
-const models = {
-    products:
-        [
-            {
-                id: 1,
-                name: 'Ft-150',
-                urlImage: 'https://raw.githubusercontent.com/mmartinezdev09/apiService/main/images/ft_150.jpeg'
-            },
-            {
-                id: 2,
-                name: 'Ft-180',
-                urlImage: 'https://raw.githubusercontent.com/mmartinezdev09/apiService/main/images/italika_180.jpeg'
-            },
-            {
-                id: 3,
-                name: 'At-110',
-                urlImage: 'https://raw.githubusercontent.com/mmartinezdev09/apiService/main/images/italika_at_110.jpg'
-            }
-        ]
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mmartinezdev09_db_user:t0znJyvgZOhy4xSl@cluster0.o5pdjxo.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri,
+    {
+        serverApi:
+        {
+            version: ServerApiVersion.v1, strict: true, deprecationErrors: true,
+        }
+    }
+);
+
+let db; //connect data base 
+
+async function connectDB() {
+    if (!db) {
+        await client.connect();
+        db = client.db("users");
+        console.log("Mongo conectado");
+    }
 }
 
-
 const ModelController = {
-
-    getModelsInventory: (req, res) => {
-        res.json(models);
+    getModelsInventory: async (req, res) => {
+        try {
+            await connectDB();
+            const result = await db.collection("models").findOne();
+            res.json(
+                {
+                    products: result.models
+                }
+            );
+        } catch (error) {
+            res.status(500).json(
+                {
+                    error: error.message
+                }
+            );
+        }
     }
 }
 
--- FeatureProduct/controllers/welcomeController.js
+++ FeatureProduct/controllers/welcomeController.js
@@ -1,48 +1,11 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://mmartinezdev09_db_user:t0znJyvgZOhy4xSl@cluster0.o5pdjxo.mongodb.net/?appName=Cluster0";
-
-const client = new MongoClient(uri,
-    {
-        serverApi:
-        {
-            version: ServerApiVersion.v1, strict: true, deprecationErrors: true,
-        }
-    }
-);
-
-let db; //connect data base 
-
-async function connectDB() {
-    if (!db) {
-        await client.connect();
-        db = client.db("users");
-        console.log("Mongo conectado");
-    }
-}
 const welcomeResponse = {
-    message: 'Bienvenido',
+    message: 'Bienvenido amigo mio',
     iconUrl: 'https://raw.githubusercontent.com/mmartinezdev09/apiService/main/images/main/icn_main.jpg'
 }
 
 const WelcomeController = {
-    getWelcomeMessage: async (req, res) => {
-        try {
-            await connectDB();
-            const result = await db.collection("users").findOne();
-            res.json(
-                {
-                    message: 'Bienvenido',
-                    iconUrl: 'https://raw.githubusercontent.com/mmartinezdev09/apiService/main/images/main/icn_main.jpg',
-                    data: result.users
-                }
-            );
-        } catch (error) {
-            res.status(500).json(
-                {
-                    error: error.message
-                }
-            );
-        }
+    getWelcomeMessage: (req, res) => {
+        res.json(welcomeResponse)
     }
 }
 
