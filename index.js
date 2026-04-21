4d8d4a7696c13650b333e9d7b65dc325971ef7a7
Marcos Martinez <mmartinez@kapital.com>
2026-04-21 10:51:10 -0600

    [FEATURE] add get all products service

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const app = express()
 const { ProductController } = require("./FeatureProduct/controllers/productController")
 const { ModelController } = require("./FeatureProduct/controllers/modelController")
 const { WelcomeController } = require("./FeatureProduct/controllers/welcomeController")
+const { AllProductsController } = require("./FeatureProduct/controllers/allProductsController")
 
 app.use(express.json())
 
 app.get('/welcome', WelcomeController.getWelcomeMessage)
 app.get('/getProducts', ModelController.getModelsInventory)
+app.get('/getAllProducts', AllProductsController.getAllProducts)
 app.post('/getProductById', ProductController.getProductById)
 const PORT = process.env.PORT || 3000
 
